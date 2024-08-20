8332f282a7b0045b5e819c80563b03d17613b37d
Ciaran R <ciaranroe1@gmail.com>
2024-08-20 18:48:38 +0100

    First Function js file and pseudocode
    js file with the function getComputerChoice

--- javaScript.js
+++ javaScript.js
@@ -1,1 +1,16 @@
-console.log(`test`)
+let getComputerChoice = () => {
+    let randomNumber = Math.floor(1+Math.random() * 3);
+    let compChoice;
+    if(randomNumber === 1){
+        compChoice = "rock"
+    }
+    else if(randomNumber === 2){
+        compChoice = "paper"
+    }
+    else{
+        compChoice = "scissors"
+    }
+    return compChoice;
+}
+
+console.log(getComputerChoice());
