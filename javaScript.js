fb3654629eb4c157abef2641a3779d48e593b317
Ciaran R <ciaranroe1@gmail.com>
2024-08-20 22:07:57 +0100

    playround function
    added human choice and playround functions

--- javaScript.js
+++ javaScript.js
@@ -1,3 +1,6 @@
+let humanScore = 0;
+let computerScore = 0;
+
 let getComputerChoice = () => {
     let randomNumber = Math.floor(1+Math.random() * 3);
     let compChoice;
@@ -13,4 +16,36 @@ let getComputerChoice = () => {
     return compChoice;
 }
 
-console.log(getComputerChoice());
+
+let getHumanChoice = () => {
+    let userChoice = prompt("Enter rock, paper or scissors").toLowerCase();
+    if(userChoice === "rock" || userChoice === "paper" || userChoice === "scissors"){
+        return userChoice;
+    }
+    else{
+        getHumanChoice();
+    }
+    return compChoice;
+}
+
+
+let playRound = (computer, human) => {
+    if(human === computer){
+        console.log(`it's a draw`);
+    }
+    else if((human === "rock" && computer === "scissors") || 
+            (human === "paper" && computer === "rock") || 
+            (human === "scissors" && computer === "paper")){
+                humanScore++;
+                console.log(`You win. ${human} beats ${computer}`);
+    }
+    else {
+        computerScore++;
+        console.log(`You Lose. ${computer} beats ${human}`);
+    }
+}
+
+const humanSelection = getHumanChoice();
+const computerSelection = getComputerChoice();
+
+console.log(playRound(computerSelection, humanSelection))
